ab937ed5e5f4aabcc17619618ec4bc3075922b3b
sahil-sahu-dev <sahilsahucode@gmail.com>
2021-11-04 13:05:17 +0530

    Categories view

--- QuickTrivia/QuickTrivia/CategoriesChoiceView.swift
+++ QuickTrivia/QuickTrivia/CategoriesChoiceView.swift
@@ -9,59 +9,65 @@ import SwiftUI
 
 struct CategoriesChoiceView: View {
     
-    //@EnvironmentObject var triviaDocument: TriviaQuestionsGame
-
-    var body: some View {
-        DTCard(color: .red)
-        
-    }
-}
-
-struct DTCard: View {
-    var color: Color
-    var body: some View {
-        if #available(iOS 15.0, *) {
-            VStack(spacing: 20) {
-                Image(systemName: "globe")
-                if #available(iOS 15.0, *) {
-                    Text("DevTechie")
-                        .font(.largeTitle)
-                        .foregroundStyle(.primary)
-                } else {
-                    // Fallback on earlier versions
-                }
-                if #available(iOS 15.0, *) {
-                    Text("DevTechie")
-                        .font(.largeTitle)
-                        .foregroundStyle(.primary)
-                } else {
-                    // Fallback on earlier versions
-                }
-                
-                if #available(iOS 15.0, *) {
-                    Text("iOS Video Courses")
-                        .foregroundStyle(.secondary)
-                } else {
-                    // Fallback on earlier versions
-                }
-                
-                if #available(iOS 15.0, *) {
-                    Text("SwiftUI  ̣ Swift")
-                        .foregroundStyle(.tertiary)
-                } else {
-                    // Fallback on earlier versions
-                }
-            }
-            .padding()
-            .background(RoundedRectangle(cornerRadius: 20).foregroundStyle(.quaternary))
-            .foregroundStyle(color)
-            .animation(.easeInOut, value: color)
-        } else {
-            // Fallback on earlier versions
-        }
+    @EnvironmentObject var triviaDocument: TriviaQuestionsGame
+    
+    var yellowColor:Color = Color(red: 255/255.0,green: 235/255.0, blue: 161/255.0)
+    var blackColor: Color = Color(red: 54/255.0, green: 54/255.0, blue: 54/255.0)
+    
+    var categories = ["General Knowledge", "Geography", "History", "Sports", "Science & Nature"]
+    var columns: [GridItem] {
+        [GridItem(.adaptive(minimum: 150, maximum: 200))]
     }
     
+    var body: some View {
+        
+            ZStack{
+                yellowColor
+                    .edgesIgnoringSafeArea(.top)
+                    .edgesIgnoringSafeArea(.bottom)
+                ScrollView{
+                    LazyVGrid(columns: columns){
+                        ForEach(categories, id: \.self) { category in
+                            
+                            NavigationLink(destination: TriviaQuestionsGameView()){
+                            
+                                CardView(category: category)
+                                .aspectRatio(9/10, contentMode: ContentMode.fill)
+                                .padding()
+                                .foregroundColor(blackColor)
+                                
+                            
+                            }.simultaneousGesture(TapGesture().onEnded {
+                                self.triviaDocument.loadNewCategory(for: category)
+                            })
+                       
+                        }
+                    }
+                }
+            }
+    }
 }
+
+
+struct CardView: View {
+    
+    var blackColor: Color = Color(red: 54/255.0, green: 54/255.0, blue: 54/255.0)
+    var category: String
+    
+    var body: some View{
+        GeometryReader{ geometry in
+        RoundedRectangle(cornerRadius: 15)
+                .overlay(Text(category)
+                            .foregroundColor(.white)
+                            .padding())
+            
+        }
+        
+    }
+
+    
+}
+
 
 struct CategoriesChoiceView_Previews: PreviewProvider {
     static var previews: some View {
